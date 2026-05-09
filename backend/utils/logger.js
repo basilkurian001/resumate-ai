253748017a09c8eaa8b02d8d7fc28907b723bb6a
basilkurian001 <gamerbasil05@gmail.com>
2026-05-09 14:46:25 +0530

    fix-savefailedfile not defined
    - added savefailedfile function
    - updated logger imports on aiService.js, resumeController.js

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,20 +1,18 @@
 import winston from "winston";
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
+import { v4 as uuidv4 } from "uuid";
 
 // Define log directory
 const logDir = path.join("logs");
 
 // Create logs folder if it doesn't exist
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir, { recursive: true });
-}
+await fs.mkdir(logDir, { recursive: true });
+
+const filesDir = path.join(logDir, "files");
 
 //create logs/files if not exist
-const filesDir = path.join(logDir, "files");
-if (!fs.existsSync(filesDir)) {
-  fs.mkdirSync(filesDir, { recursive: true });
-}
+await fs.mkdir(filesDir, { recursive: true });
 
 const logger = winston.createLogger({
   level: "error",
@@ -27,4 +25,25 @@ const logger = winston.createLogger({
   ],
 });
 
-export default logger;
+async function saveFailedFile(fileData) {
+    let fileName = uuidv4();  //generate uuid filename
+    fileName = fileName + path.extname(fileData.originalname)
+
+    // Full file path
+    const filePath = path.join(filesDir, fileName);
+
+    // Save file
+    await fs.writeFile(filePath, fileData.buffer);
+
+    // Return saved data
+    const savedFile = {
+      fileName: fileName
+    };
+    return savedFile;
+}
+
+//export default logger;
+export{
+  saveFailedFile,
+  logger
+}
